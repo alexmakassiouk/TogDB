85b4cc45d39498e99d027a7404ddaf7d71109e3d
ComputasAlex <almax@computas.com>
2023-03-29 15:21:26 +0200

    Kinda starting to work

--- src/search.py
+++ src/search.py
@@ -1,7 +1,7 @@
 import sqlite3
 from datetime import datetime
 
-con = sqlite3.connect("tog.db")
+con = sqlite3.connect("src/tog.db")
 cur = con.cursor()
 
 def search_main():
@@ -14,19 +14,26 @@ def search_main():
 def construct_query(dep, des, date: str, time: str):
     day, month, year = date.split(".")
     hour, minute = time.split(":")
-    dt = datetime.combine(datetime.date(datetime.year(int(year)), datetime.month(int(month)), datetime.day(int(day))), datetime.time(datetime.hour(int(hour)), datetime.minute(int(minute))))
+    combined_date_time = date + " " + time
+    try:
+        dt = datetime.strptime(combined_date_time, '%d.%m.%y %H:%M')
+    except ValueError:
+        print("Invalid date!")
+        return
     weekday = dt.isoweekday()
 
     sql = f"""
-    SELECT tu.togruteID, ts.jernbanestasjonNavn, ts.avgangstid, t.endestasjon, t.ankomsttid
-        FROM togrute_ukedager AS tu
-        INNER JOIN togrute AS t ON (tu.togruteID = t.ID)
-        INNER JOIN togrute_stoppestasjon AS ts ON (t.ID = ts.togruteID)
-        WHERE tu.ukedagID = {weekday}
-        AND ts.jernbanestasjonNavn = '{dep}'
+        SELECT t.ID, t.startstasjon, t.avgangstid, ts.jernbanestasjonNavn, ts.avgangstid, t.endestasjon, t.ankomsttid, tu.ukedagID
+        FROM togrute AS t INNER JOIN togrute_stoppestasjon AS ts ON (t.ID = ts.togruteID)
+        INNER JOIN togrute_ukedager AS tu ON (tu.togruteID = t.ID)
+        WHERE ((tu.ukedagID = {weekday}) OR tu.ukedagID = {weekday+1})
+        AND (ts.jernbanestasjonNavn = '{dep}' OR ts.jernbanestasjonNavn = '{des}')
+        ORDER BY tu.ukedagID, t.ID, ts.avgangstid
     """
-
+    rows = []
     for row in cur.execute(sql):
+        rows.append(row)
         print(row)
+    
 
-construct_query("Steinkjer", "Bodø", "29.03.23", "07:00")
+construct_query("Fauske", "Bodø", "29.03.23", "07:00")
