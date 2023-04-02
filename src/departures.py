e49723b05c06f48c59ceb36b451336a1d7826105
ComputasAlex <almax@computas.com>
2023-04-02 21:40:20 +0200

    Comments in departues OK

--- src/departures.py
+++ src/departures.py
@@ -11,26 +11,28 @@ def departures_main():
     construct_query(station_input, weekday_input)
 
 def construct_query(station, weekday):
-    #Decide on a format for station and weekday
-    sql = f"""
+
+    # Query all stations along a trainroute
+    sql_middle_stations = f"""
         SELECT 
           tu.togruteID, ts.jernbanestasjonNavn, ts.avgangstid, t.endestasjon, t.ankomsttid
         FROM 
           togrute_ukedager AS tu
           INNER JOIN 
             togrute AS t ON (tu.togruteID = t.ID)
-            INNER JOIN 
-              togrute_stoppestasjon AS ts ON (t.ID = ts.togruteID)
+          INNER JOIN 
+            togrute_stoppestasjon AS ts ON (t.ID = ts.togruteID)
         WHERE 
           tu.ukedagID = ?
-          AND 
-          ts.jernbanestasjonNavn = ?
+          AND ts.jernbanestasjonNavn = ?
         """
     
     rows = []
-    for row in cur.execute(sql, (weekday, station)):
+    for row in cur.execute(sql_middle_stations, (weekday, station)):
+        # First column: 0 for start station, 1 for middle station, 2 for end station
         rows.append((1, *row))
 
+    # Query all starting stations (Trondheim S and Mo i Rana)
     sql_start_stations = f"""
     SELECT 
       tu.togruteID, t.startstasjon, t.avgangstid, t.endestasjon, t.ankomsttid
@@ -43,8 +45,10 @@ def construct_query(station, weekday):
       AND t.startstasjon = ?
     """
     for row in cur.execute(sql_start_stations, (weekday, station)):
+        # First column: 0 for start station, 1 for middle station, 2 for end station
         rows.append((0, *row))
 
+    # Query for all end stations (Trondheim S and Bodø)
     sql_end_stations = f"""
     SELECT 
       tu.togruteID, t.endestasjon, t.ankomsttid
@@ -57,8 +61,10 @@ def construct_query(station, weekday):
       AND t.endestasjon = ?
     """
     for row in cur.execute(sql_end_stations, (weekday, station)):
+        # First column: 0 for start station, 1 for middle station, 2 for end station
         rows.append((2, *row))
 
+    # Nice display for user. Show all train routes at a station, even the ones that just arrive
     for row in rows:
         if row[0] == 0 or row[0] == 1:
             print("Togrute", row[1], ":", row[2], "kl.", row[3], "-", row[4], "kl.", row[5])
