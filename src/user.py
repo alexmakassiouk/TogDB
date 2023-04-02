f4d74e0b0ebe7dd16bdfdfda560d25ebbb554052
ComputasAlex <almax@computas.com>
2023-04-02 21:56:20 +0200

    Error handling and comments in user - Validate email - Validate phonenumber

--- src/user.py
+++ src/user.py
@@ -1,20 +1,35 @@
 import sqlite3
+import re
 
 con = sqlite3.connect("tog.db")
 cur = con.cursor()
 
+# Create user
 def user_main():
     name = input("What is your name? ")
     email = input("What is your e-mail? ")
+    while not is_valid_email(email):
+        email = input("Invalid email. Please try again: ")
     phone = input("And lastly, what is your phone number? ")
+    while not is_valid_phone(phone):
+        phone = input("Invalid phone. Please try again: ") 
     post_user(name, email, phone)
 
+def is_valid_email(email):
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    if re.fullmatch(regex, email):
+        return True
+    return False
     
-
+def is_valid_phone(phone: str):
+    if phone.isdigit():
+        return True
+    return False
 def post_user(name, email, phone):
     sql = f"""
     INSERT INTO kunde(navn, epost, mobilnummer) VALUES(?, ?, ?)
     """
+    # Cannot have already existing phone and email. Those values are UNIQUE
     try:
         cur.execute(sql, (name, email, phone))
         con.commit()
